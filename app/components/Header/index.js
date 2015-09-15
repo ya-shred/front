815f59819b533f7b38d32322ea1961938a8771d5
Rahnar <rea-90@mail.ru>
2015-09-15 03:16:36 +0300

    Написал пример различных событий, такие как клик и изменение onChange на Ecma6 , сразу привыкайте писать ecma6

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -2,10 +2,26 @@ import React, { PropTypes, Component } from 'react';
 import './index.styl';
 
 class Header extends Component {
+	constructor() {
+		super();
+		this.state = { count: 0, value: "" };
+		this._click = this._click.bind(this);
+		this.value = this.value.bind(this);
+	}
+	_click() {
+		this.setState({ count: this.state.count + 1})
+	}
+	value(e) {
+		this.setState({ value: e.target.value })
+	}
 
 	render() {
+		var count = this.state.count;
+		var val =  this.state.value;
 		return <header className="header">
-			<h1>Hello - <span className="shred">Shred!</span></h1>
+			 <input type="button" value={"value " + count } onClick={this._click} />
+			<input type="text"  onChange={this.value}/>
+			<div>{val}</div>
 		</header>
 	}
 
