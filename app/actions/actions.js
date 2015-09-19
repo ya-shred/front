67442be5fad35fd35c4a8cfbf93f057c1fa60d8e
Rahnar <rea-90@mail.ru>
2015-09-19 13:38:24 +0300

    package.json

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -3,36 +3,14 @@ import { ADD_ITEM, REMOVE_ITEM, DECREASE_ITEM, INCREASE_ITEM }  from '../constan
 
 const AppActions = {
 
+
 	addItem: function (item) {
 		AppDispatcher.handleViewAction({
 			actionType: ADD_ITEM,
 			item: item
 		});
-	},
-
-	removeItem: function (index) {
-
-		AppDispatcher.handleViewAction({
-			actionType: REMOVE_ITEM,
-			index: index
-		});
-	},
-
-	increaseItem: function (index) {
-
-		AppDispatcher.handleViewAction({
-			actionType: INCREASE_ITEM,
-			index: index
-		})
-	},
-
-	decreaseItem: function (index) {
-
-		AppDispatcher.handleViewAction({
-			actionType: DECREASE_ITEM,
-			index: index
-		})
 	}
+
 };
 
 module.exports = AppActions;
