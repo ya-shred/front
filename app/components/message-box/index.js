6b7af3346e8f7abb3a4d4a5d3710644fba8351b8
Dima Malofeev <dim2k2006@yandex.ru>
2015-09-28 23:21:53 +0300

    dynamic textarea height

--- app/components/message-box/index.js
+++ app/components/message-box/index.js
@@ -3,27 +3,29 @@ import Actions from '../../actions/actions';
 import AppStore from '../../stores/app-store';
 import './index.styl';
 
+let MarkdownTextarea = require('react-markdown-textarea');
+
 export default class MessagesBox extends React.Component {
     state = {
-        userInput: ''
-    }
-
-    handleChange = (e) => {
-        this.setState({userInput: e.target.value});
+        value: ''
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        var text = React.findDOMNode(this.refs.text).value.trim();
-        Actions.sendMessage(text);
+        Actions.sendMessage(this.state.value);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            value: e
+        });
     }
 
     render() {
         return (
             <div className="input__container">
                 <form className="input__form" onSubmit={this.handleSubmit}>
-                    <input type="text" ref="text" value={this.state.userInput} onChange={this.handleChange} />
-                    <button type="submit" className="input__button">Send</button>
+                    <MarkdownTextarea noPreview saveButtonText='Send' onChange={this.handleChange} />
                 </form>
             </div>
         );
