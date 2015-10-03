76a94e2b10d729a21dbaf835e5829cc3e741cf03
Dima Malofeev <dim2k2006@yandex.ru>
2015-10-03 19:21:10 +0300

    close #14 добавили вывод картинки и мени пользователя

--- app/components/logout/index.js
+++ app/components/logout/index.js
@@ -4,14 +4,45 @@ import UserStore from '../../stores/user';
 import './index.styl';
 
 
+var getUserInfo = () => {
+    return {
+        user: UserStore.getUserInfo()
+    }
+};
+
+
 export default class Logout extends React.Component {
+	constructor() {
+        super();
+        this.state = getUserInfo();
+    }
+
+
 
     handleClick = (event) => {
         LogoutActions.logout();
     };
+    
+
+    componentDidMount() {
+        UserStore.addChangeListener(this.onChange);
+    }
+
+    onChange = () => {
+        this.setState(getUserInfo());
+        console.log(this.state);
+    }
+
 
     render() {
-        return <button onClick={this.handleClick} className="logout">Выйти</button>
+        return <div className="logout">
+        	<div className="logout__wrapper">
+        		<img className="logout__img" src={this.state.user.avatarUrl}/>
+        	</div><div 
+        	className="logout__name">{this.state.user.displayName}</div><button 
+        	onClick={this.handleClick} className="logout__button"></button>
+        </div>;
+        
     }
 
 };
